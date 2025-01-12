25a67a591f8d70af074a8f48015f51ce4daec7db
Basanthi <chavalibasanthi@gmail.com>
2025-01-12 10:02:32 +0530

    Nine table program using printf

---
+++ src/ninetable.java
@@ -0,0 +1,16 @@
+public class ninetable {
+    public static void main (String[] args) {
+        int t = 9;
+        System.out.printf("9 * 1 = %d\n", t * 1);
+        System.out.printf("9 * 2 = %d\n", t * 2);
+        System.out.printf("9 * 3 = %d\n", t * 3);
+        System.out.printf("9 * 4 = %d\n", t * 4);
+        System.out.printf("9 * 5 = %d\n", t * 5);
+        System.out.printf("9 * 6 = %d\n", t * 6);
+        System.out.printf("9 * 7 = %d\n", t * 7);
+        System.out.printf("9 * 8 = %d\n", t * 8);
+        System.out.printf("9 * 9 = %d\n", t * 9);
+        System.out.printf("9 * 10 = %d\n", t * 10);
+    }
+
+}
