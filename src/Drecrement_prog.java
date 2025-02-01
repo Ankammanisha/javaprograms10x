ffef84ceaacea7434d83f4cbe4b67d43699f2ee3
Basanthi <chavalibasanthi@gmail.com>
2025-02-01 15:14:15 +0530

    Decrement program

---
+++ src/Drecrement_prog.java
@@ -0,0 +1,7 @@
+public class Drecrement_prog {
+    public static void main(String[] args) {
+        int a=20;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }
+}
