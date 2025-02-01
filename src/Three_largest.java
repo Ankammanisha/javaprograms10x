da1337e1e0ae5e51e325833519b9c03be69c48e3
Basanthi <chavalibasanthi@gmail.com>
2025-02-01 14:59:38 +0530

    Largest of three numbers

---
+++ src/Three_largest.java
@@ -0,0 +1,7 @@
+public class Three_largest {
+    public static void main(String[] args) {
+        int a=10,b=20,c=15;
+        String large=((a>b&&a>c)?a+" is largest":(b>c)?b+" is largest":c+" is largest");
+        System.out.println(large);
+    }
+}
