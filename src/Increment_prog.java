9095de51ccd41f864e563e0276bb4a665a1838b7
Basanthi <chavalibasanthi@gmail.com>
2025-02-01 15:09:48 +0530

    Increment program

---
+++ src/Increment_prog.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Increment_prog {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+
+
+    }
+}
